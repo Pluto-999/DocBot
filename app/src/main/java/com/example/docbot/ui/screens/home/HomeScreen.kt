7f5762ab1a0aa709abd7c3684080d6e2c2b10ccb
Pluto-999 <adamw0909@gmail.com>
2026-02-08 15:08:25 +0000

    Add button for creating conversations to home screen

--- app/src/main/java/com/example/docbot/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/docbot/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,22 @@
 package com.example.docbot.ui.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -15,10 +24,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3ExpressiveApi::class
+)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel<HomeViewModel>()
@@ -61,18 +75,35 @@ fun HomeScreen(
                 modifier = Modifier
             )
         },
-        bottomBar = {
-            BottomAppBar {
-                Text("Bottom app bar")
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Button(
+                onClick = ({ /* Create Conversation */}),
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .padding(12.dp),
+                contentPadding = PaddingValues(vertical = 16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                    Spacer(modifier = Modifier
+                        .width(8.dp))
+                    Text("New Conversation")
+                }
             }
-        },
-        modifier = Modifier.fillMaxSize()){ innerPadding ->
-        Text(
-            text = "Hello",
-            modifier = Modifier.padding(innerPadding)
-        )
-
-
+        }
     }
-
 }
