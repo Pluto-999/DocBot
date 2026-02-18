4b1878f8f28abaccae3e9e49f73a05b9a92ea4bc
Pluto-999 <adamw0909@gmail.com>
2026-02-18 13:46:45 +0000

    Move ObjectBox init to App class rather than Activity to fix configuration change bug

--- app/src/main/java/com/example/docbot/App.kt
+++ app/src/main/java/com/example/docbot/App.kt
@@ -1,7 +1,13 @@
 package com.example.docbot
 
 import android.app.Application
+import com.example.docbot.data.ObjectBox
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ObjectBox.init(this)
+    }
+}
--- app/src/main/java/com/example/docbot/MainActivity.kt
+++ app/src/main/java/com/example/docbot/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.docbot.data.ObjectBox
 import com.example.docbot.ui.navigation.Navigation
 import com.example.docbot.ui.theme.DocBotTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +12,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ObjectBox.init(this)
         enableEdgeToEdge()
         setContent {
             DocBotTheme {
