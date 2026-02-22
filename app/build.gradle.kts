a14a67a591358153fef650f1ac94550a211702d7
Pluto-999 <adamw0909@gmail.com>
2026-02-22 19:29:45 +0000

    Fix navigation bug where the first chat page navigated to would always be shown in subsequent navigations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.work.runtime)
     implementation(libs.androidx.hilt.common)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material.icons.extended)
--- app/src/main/java/com/example/docbot/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/docbot/ui/navigation/Navigation.kt
@@ -1,9 +1,10 @@
 package com.example.docbot.ui.navigation
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation3.runtime.NavEntry
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import com.example.docbot.ui.screens.chat.ChatScreen
 import com.example.docbot.ui.screens.home.HomeScreen
@@ -13,28 +14,21 @@ fun Navigation() {
     val backStack = rememberNavBackStack(HomePage)
 
     NavDisplay(
+        entryDecorators = listOf(
+            rememberSaveableStateHolderNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator()
+        ),
         backStack = backStack,
-        entryProvider = { key ->
-            when (key) {
-                is HomePage -> {
-                    NavEntry(key) {
-                        HomeScreen(
-                            onConversationNavigate = {
-                                backStack.add(ChatPage(id = it))
-                            }
-                        )
+        entryProvider = entryProvider {
+            entry<HomePage> {
+                HomeScreen(
+                    onConversationNavigate = {
+                        backStack.add(ChatPage(id = it))
                     }
-                }
-                is ChatPage -> {
-                    NavEntry(key) {
-                        ChatScreen(key.id)
-                    }
-                }
-                else -> {
-                    NavEntry(key) {
-                        Text("Unknown route")
-                    }
-                }
+                )
+            }
+            entry<ChatPage> {
+                key -> ChatScreen(key.id)
             }
         }
     )
