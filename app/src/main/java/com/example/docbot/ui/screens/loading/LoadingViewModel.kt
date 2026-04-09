3031d712ff4e69dfd2532be352fe78a5c29d5fca
Pluto-999 <adamw0909@gmail.com>
2026-04-09 08:46:01 +0100

    Alter initialisation of model file to include the SLM only

--- app/src/main/java/com/example/docbot/ui/screens/loading/LoadingViewModel.kt
+++ app/src/main/java/com/example/docbot/ui/screens/loading/LoadingViewModel.kt
@@ -15,28 +15,20 @@ class LoadingViewModel @Inject constructor(
 ): ViewModel() {
 
     suspend fun initialise(context: Context) = withContext(Dispatchers.IO) {
-        initialiseModelFiles(context)
+        initialiseModelFile(context)
         messageGenerator.initialiseEngine()
     }
 
-    private fun initialiseModelFiles(context: Context) {
+    private fun initialiseModelFile(context: Context) {
         val modelDirectory = File(context.filesDir, "slm")
         modelDirectory.mkdirs()
 
-        val filesToCopy = listOf(
-            "embeddinggemma-300M_seq1024_mixed-precision.tflite",
-            "embeddinggemma-300M_seq2048_mixed-precision.tflite",
-            "gemma-3n-E2B-it-int4.litertlm",
-            "sentencepiece.model"
-        )
-
-        for (fileName in filesToCopy) {
-            val source = File("/data/local/tmp/slm/$fileName")
-            val destination = File(modelDirectory, fileName)
-            if (!destination.exists() || destination.length() != source.length()) {
-                File("/data/local/tmp/slm/$fileName")
-                    .copyTo(target = destination, overwrite = true)
-            }
+        val fileName = "gemma-3n-E2B-it-int4.litertlm"
+        val source = File("/data/local/tmp/slm/$fileName")
+        val destination = File(modelDirectory, fileName)
+        if (!destination.exists() || destination.length() != source.length()) {
+            File("/data/local/tmp/slm/$fileName")
+                .copyTo(target = destination, overwrite = true)
         }
     }
 }
